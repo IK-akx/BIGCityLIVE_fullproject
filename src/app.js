15417b50e6235333b13acf7675a7344609dcb814
IK-akx <kustaev.iskander12@gmail>
2026-02-08 13:10:20 +0500

    Implemented /resource CRUD (Tickets) with owner checks

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ app.use(express.json());
 // routes
 const authRoutes = require("./routes/auth.routes");
 const usersRoutes = require("./routes/users.routes");
+const resourceRoutes = require("./routes/resource.routes");
 
 app.use("/", authRoutes);
 app.use("/", usersRoutes);
+app.use("/", resourceRoutes);
 
 //test
 app.get("/health", (req, res) => {
